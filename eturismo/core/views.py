d3e0b4cc618c8a51034e02a55d17e9705a925dd5
Heverton13 <evertinho10.hg@gmail.com>
2017-12-06 15:12:44 -0300

    Ajustes provisorios no site para intereção

--- eturismo/core/views.py
+++ eturismo/core/views.py
@@ -6,7 +6,13 @@ def index(request):
 	return render(request, 'index.html')
 
 def informacoes(request):
+	#receber via get o nome do destino digitado
+	# pesquisar com GET o destino
+	# enviar para a página informações o objetco recuperado via context
+
+
 	return render(request, 'informacoes.html')
+
 # Create your views here.
 
 

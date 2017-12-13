f00416d9457ca907f7a0396a575adfb9ce523ec9
Uliisses04 <ulisses.iven@gmail.com>
2017-12-13 15:58:23 -0300

    Atualizacao do informacao

--- eturismo/core/views.py
+++ eturismo/core/views.py
@@ -8,7 +8,42 @@ def index(request):
 	return render(request, 'index.html')
 
 def informacoes(request):
+	id_informacoes = request.GET.get("id")
 	
+	id_municipioinfo = request.GET.get("id")
+	municipioinfo = Municipio.objects.get(id=id_municipioinfo)
+
+	id_dicainfo = request.GET.get("id")
+	dicainfo = Dica.objects.get(id=id_dicainfo)
+
+	id_passeioinfo = request.GET.get("id")
+	passeioinfo = Passeio.objects.get(id=id_passeioinfo)
+
+	id_promocaoinfo = request.GET.get("id")
+	promocaoinfo = Promocao.objects.get(id=id_promocaoinfo)
+
+	id_avaliacaoinfo = request.GET.get("id")
+	avaliacaoinfo = Avaliacao.objects.get(id=id_avaliacaoinfo)
+
+	id_enderecoinfor = request.GET.get("id")
+	enderecoinfor = Endereco.objects.get(id=id_enderecoinfo)
+
+	id_contatoinfor = request.GET.get("id")
+	contatoinfor = Contato.objects.get(id=id_contatoinfo)
+
+	id_tipodicainfor = request.GET.get("id")
+	tipodicainfor = Tipo_Dica.objects.get(id=id_tipodicainfo)
+
+	context = {
+	'municipioinfo': municipioinfo,
+	'dicainfo': dicainfo,
+	'passeioinfo': passeioinfo,
+	'promocaoinfo': promocaoinfo,
+	'avaliacaoinfo': avaliacaoinfo,
+	'enderecoinfor': enderecoinfor,
+	'contatoinfor': contatoinfor,
+	'tipodicainfor': tipodicainfor,
+}
 	return render(request, 'informacoes.html')
 
 def lista_destinos(request):
